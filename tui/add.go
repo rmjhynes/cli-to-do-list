47f4a6fdd5487d17fcb985bf0fda62a448e3f9a6
Ryan Hynes <rmjhynes@gmail.com>
2024-10-27 11:25:46 +0000

    Added function to identify the last task ID in the list.

--- tui/add.go
+++ tui/add.go
@@ -5,11 +5,14 @@ import (
 	"cli-to-do-list/constants"
 	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 func AddRecord() {
+	findPreviousTaskID()
+
 	f, err := os.OpenFile(constants.TaskData, os.O_APPEND|os.O_WRONLY, 0777)
 	if err != nil {
 		log.Fatal(err)
@@ -56,5 +59,28 @@ func AddRecord() {
 
 	if err := w.Error(); err != nil {
 		log.Fatalln("error writing csv:", err)
+	}
+}
+
+func findPreviousTaskID() (string, error) {
+	f, err := os.Open(constants.TaskData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(f)
+	var lastRecord string
+
+	for {
+		rec, err := r.Read()
+
+		if err == io.EOF {
+			fmt.Println("The last record: ", lastRecord)
+			return lastRecord, nil
+		}
+		if err != nil {
+			return "", nil
+		}
+		lastRecord = rec[0]
 	}
 }
